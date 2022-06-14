866d02636d01f31e2ff46786eae9870f8a51565e
Uttam Shrestha <61283364+uttamstha123@users.noreply.github.com>
2022-06-14 22:01:30 +0530

    Add files via upload

---
+++ script/gsapScript.js
@@ -0,0 +1,46 @@
+gsap.registerPlugin(ScrollTrigger);
+
+let word = document.querySelector(".words");
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".container",
+    // toggleActions: "play complete reverse reset",
+    scrub: 1,
+    start: "top top",
+    end: "+=1000",
+    pin: true,
+    // markers: true
+  },
+});
+gsap.set("h1", {
+  scale: 4.5,
+  transform: "translateY(100%)",
+});
+tl.to(".hero-section__title", { duration: 2, scale: 1, ease: "sine.out" })
+  .to(".hero-section__title", {
+    duration: 2,
+    ease: "sine.out",
+    transform: "translateY(0)",
+  })
+  .from(
+    ".hero-section h2",
+    {
+      duration: 1,
+      opacity: 0,
+      ease: "sine.out",
+    },
+    "sameTime"
+  )
+  .from(
+    ".nav",
+    {
+      duration: 1,
+      opacity: 0,
+      transform: "translateY(-100%)",
+      ease: "sine.out",
+      onComplete: () => {
+        word.classList.add("animateWord");
+      },
+    },
+    "sameTime-=0.5"
+  );
