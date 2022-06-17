b0217affb5e4a3eb3f009ad6a97fdac1c7613a83
Uttam Shrestha <61283364+uttamstha123@users.noreply.github.com>
2022-06-17 21:04:01 +0530

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -4,6 +4,7 @@ const wePlanLink = document.querySelectorAll(".weplan-link");
 wePlanLink.forEach((link) => {
   link.addEventListener("click", (e) => {
     let id = e.target.id;
+    console.log(id);
     window.sessionStorage.clear();
     window.sessionStorage.setItem("ID", id);
   });
